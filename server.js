241841883d1510df7b9a04403ad2097a57e5ffb5
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-10-14 21:50:21 +0600

    bug fix

--- server.js
+++ server.js
@@ -13,12 +13,12 @@ app.use(cors());
 app.use(morgan("dev"));
 cloudinary();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });
+// }
 
 app.use("/user", userRouter);
 app.use("/product", productRouter);
