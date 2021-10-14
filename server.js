8b45b0bec44537e7983c5207704b05642bf73dac
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-10-14 22:11:11 +0600

    try to fix

--- server.js
+++ server.js
@@ -4,8 +4,8 @@ const cors = require("cors");
 const morgan = require("morgan");
 const db = require("./config/db");
 const cloudinary = require("./config/cloudinary");
-// const productRouter = require("./routers/productRouter");
 const userRouter = require("./routers/userRouter");
+// const productRouter = require("./routers/productRouter");
 
 const app = express();
 app.use(express.json());
@@ -13,12 +13,12 @@ app.use(cors());
 app.use(morgan("dev"));
 cloudinary();
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-//   });
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.use("/user", userRouter);
 // app.use("/product", productRouter);
