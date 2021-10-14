7fc843d63d58388cf835b0a1133ce1932f84c9d2
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-10-14 22:16:25 +0600

    product router problem

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan = require("morgan");
 const db = require("./config/db");
 const cloudinary = require("./config/cloudinary");
 const userRouter = require("./routers/userRouter");
-// const productRouter = require("./routers/productRouter");
+const productRouter = require("./routers/productRouter");
 
 const app = express();
 app.use(express.json());
@@ -13,15 +13,8 @@ app.use(cors());
 app.use(morgan("dev"));
 cloudinary();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-  });
-}
-
 app.use("/user", userRouter);
-// app.use("/product", productRouter);
+app.use("/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("A MERN stack ecommerce app");
