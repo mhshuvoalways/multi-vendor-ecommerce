51c4a767ddd0f3737578f2d1ffb63b113dd0f42d
Mahamudul Hasan Shuvo <mlhnshuvo@gmail.com>
2021-10-14 22:04:51 +0600

    fix

--- server.js
+++ server.js
@@ -1,18 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const morgan = require("morgan");
-// const db = require("./config/db");
 const cloudinary = require("./config/cloudinary");
-// const productRouter = require("./routers/productRouter");
-// const userRouter = require("./routers/userRouter");
-const mongoose = require("mongoose");
+const productRouter = require("./routers/productRouter");
+const userRouter = require("./routers/userRouter");
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(express.json());
 app.use(cors());
-// app.use(morgan("dev"));
+app.use(morgan("dev"));
 cloudinary();
 
 // if (process.env.NODE_ENV === "production") {
@@ -22,20 +21,20 @@ cloudinary();
 //   });
 // }
 
-// app.use("/user", userRouter);
-// app.use("/product", productRouter);
+app.use("/user", userRouter);
+app.use("/product", productRouter);
 
 app.get("/", (req, res) => {
   res.send("A MERN stack ecommerce app");
 });
 
 mongoose
-.connect(process.env.MONGODB_URL)
-.then(() => {
-  app.listen(port, () => {
-    console.log(`App listening at : ${port}`);
+  .connect(process.env.MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`App listening at : ${port}`);
+    });
+  })
+  .catch(() => {
+    console.log("Server error occurred");
   });
-})
-.catch(() => {
-  console.log("Server error occurred");
-});
