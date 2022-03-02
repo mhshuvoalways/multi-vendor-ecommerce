58396874331cca4d91ebbad586b18227a1100509
mlhnshuvo <mlhnshuvo@gmail.com>
2022-03-02 10:34:02 +0600

    port and secure add in transporter

--- mail/transporter.js
+++ mail/transporter.js
@@ -3,6 +3,8 @@ const nodemailer = require("nodemailer");
 module.exports = (sendto, templateName, name, link) => {
   const transporter = nodemailer.createTransport({
     service: "gmail",
+    port: 587,
+    secure: false,
     auth: {
       user: process.env.node_mailer_user,
       pass: process.env.node_mailer_password,
