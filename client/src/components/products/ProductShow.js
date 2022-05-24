a647de68e08dba67d2d021222debe1693e558037
mlhnshuvo <mlhnshuvo@gmail.com>
2022-05-24 11:22:23 +0600

    trying to solve

--- client/src/components/products/ProductShow.js
+++ client/src/components/products/ProductShow.js
@@ -24,6 +24,9 @@ const ProductModal = ({ state, modalHandler, productReducer }) => {
       productReducer.products &&
         productReducer.products.find((el) => el._id === state.id)
     );
+  }, [productReducer.products, state.id]);
+
+  useEffect(() => {
     const findCart = cartReducer.cart.find(
       (el) => el.productId === (products && products._id)
     );
@@ -36,122 +39,124 @@ const ProductModal = ({ state, modalHandler, productReducer }) => {
   );
 
   return (
-    <Modal
-      modal={state.modal}
-      modalHandler={modalHandler}
-      modalTitle={products && products.name}
-    >
+    <div>
       {products && products ? (
-        <div className="w-11/12 my-20 md:flex gap-20 m-auto">
-          <div className="w-full md:w-2/5">
-            <img
-              src={products.image[0].url}
-              alt=""
-              className="w-full border p-5"
-            />
-          </div>
-          <div className="my-10 md:my-0 md:w-2/4 w-full">
-            <p className="mb-2 text-3xl">{products.name}</p>
-            <div className="flex">
-              <p className="text-2xl">${products.salePrice}</p>
-              <span className="mx-2 text-2xl">-</span>
-              <p className="line-through text-2xl">${products.regularPrice}</p>
-            </div>
-            <div className="mt-2">
-              <ReactStars
-                {...{
-                  size: 20,
-                  value: products.rating,
-                  count: 5,
-                  activeColor: "red",
-                  a11y: true,
-                  isHalf: true,
-                  edit: false,
-                }}
+        <Modal
+          modal={state.modal}
+          modalHandler={modalHandler}
+          modalTitle={products && products.name}
+        >
+          <div className="w-11/12 my-20 md:flex gap-20 m-auto">
+            <div className="w-full md:w-2/5">
+              <img
+                src={products.image[0].url}
+                alt=""
+                className="w-full border p-5"
               />
             </div>
-            <p className="mt-5 text-base">{parse(products.description)}</p>
-            <p className="border-solid bg-gray-100 border-2 my-10"></p>
-            <div className="flex gap-4 items-center flex-wrap">
-              <div className="flex gap-5 border-solid border border-gray-300 cursor-pointer p-2">
-                <p
-                  onClick={() => {
-                    if (count > 1) {
-                      setCount(count - 1);
-                    }
-                  }}
-                >
-                  -
+            <div className="my-10 md:my-0 md:w-2/4 w-full">
+              <p className="mb-2 text-3xl">{products.name}</p>
+              <div className="flex">
+                <p className="text-2xl">${products.salePrice}</p>
+                <span className="mx-2 text-2xl">-</span>
+                <p className="line-through text-2xl">
+                  ${products.regularPrice}
                 </p>
-                <p>{count}</p>
-                <p onClick={() => setCount(count + 1)}>+</p>
               </div>
-              {userReducer.isAuthenticate ? (
-                <button
-                  className={
-                    cart && cart.productId === products._id
-                      ? "bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-not-allowed p-3 border border-gray-100 text-sm"
-                      : "bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-pointer p-3 border border-gray-100 text-sm"
-                  }
-                  onClick={() => {
-                    dispatch(
-                      addCart(products._id, {
-                        quantity: count,
-                      })
-                    );
+              <div className="mt-2">
+                <ReactStars
+                  {...{
+                    size: 20,
+                    value: products.rating,
+                    count: 5,
+                    activeColor: "red",
+                    a11y: true,
+                    isHalf: true,
+                    edit: false,
                   }}
-                >
-                  ADD TO CART
-                </button>
-              ) : (
-                <Link to="/login">
-                  <button className="bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-pointer p-3 border border-gray-100 text-sm">
+                />
+              </div>
+              <p className="mt-5 text-base">{parse(products.description)}</p>
+              <p className="border-solid bg-gray-100 border-2 my-10"></p>
+              <div className="flex gap-4 items-center flex-wrap">
+                <div className="flex gap-5 border-solid border border-gray-300 cursor-pointer p-2">
+                  <p
+                    onClick={() => {
+                      if (count > 1) {
+                        setCount(count - 1);
+                      }
+                    }}
+                  >
+                    -
+                  </p>
+                  <p>{count}</p>
+                  <p onClick={() => setCount(count + 1)}>+</p>
+                </div>
+                {userReducer.isAuthenticate ? (
+                  <button
+                    className={
+                      cart && cart.productId === products._id
+                        ? "bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-not-allowed p-3 border border-gray-100 text-sm"
+                        : "bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-pointer p-3 border border-gray-100 text-sm"
+                    }
+                    onClick={() => {
+                      dispatch(
+                        addCart(products._id, {
+                          quantity: count,
+                        })
+                      );
+                    }}
+                  >
                     ADD TO CART
                   </button>
-                </Link>
-              )}
-              {userReducer.isAuthenticate ? (
-                <i
-                  className={
-                    idCheck &&
-                    idCheck.authorId === userReducer.user._id &&
-                    idCheck.productId === products._id
-                      ? "fas fa-heart text-2xl cursor-not-allowed text-red-800"
-                      : "far fa-heart text-2xl cursor-pointer"
-                  }
-                  onClick={() => dispatch(addWishList(products._id))}
-                ></i>
-              ) : (
-                <Link to="/login">
-                  <i className="far fa-heart text-2xl cursor-pointer"></i>
-                </Link>
-              )}
-            </div>
-            <div className="mt-5">
-              <p>Categories : {products.category}</p>
-              <p className="flex gap-1">
-                <p>Tags:</p>
-                {products.tags.map((el) => (
-                  <p key={el._id}>{el.name}</p>
-                ))}
-              </p>
-              <ProductShare products={products} productUrl={products._id} />
-            </div>
-            <p className="border-solid bg-gray-100 border-2 my-10"></p>
-            <div className="border-solid border-2 border-gray-100">
-              <Link to={"/shop/" + products.author._id && products.author._id}>
-                <p className="p-2">Sold by</p>
-                <p className="text-xl p-2">
-                  {products.author.storeName && products.author.storeName}
+                ) : (
+                  <Link to="/login">
+                    <button className="bg-gray-800 hover:bg-purple-600 text-white w-40 cursor-pointer p-3 border border-gray-100 text-sm">
+                      ADD TO CART
+                    </button>
+                  </Link>
+                )}
+                {userReducer.isAuthenticate ? (
+                  <i
+                    className={
+                      idCheck &&
+                      idCheck.authorId === userReducer.user._id &&
+                      idCheck.productId === products._id
+                        ? "fas fa-heart text-2xl cursor-not-allowed text-red-800"
+                        : "far fa-heart text-2xl cursor-pointer"
+                    }
+                    onClick={() => dispatch(addWishList(products._id))}
+                  ></i>
+                ) : (
+                  <Link to="/login">
+                    <i className="far fa-heart text-2xl cursor-pointer"></i>
+                  </Link>
+                )}
+              </div>
+              <div className="mt-5">
+                <p>Categories : {products.category}</p>
+                <p className="flex gap-1">
+                  <p>Tags:</p>
+                  {products.tags.map((el) => (
+                    <p key={el._id}>{el.name}</p>
+                  ))}
                 </p>
-              </Link>
+                <ProductShare products={products} productUrl={products._id} />
+              </div>
+              <p className="border-solid bg-gray-100 border-2 my-10"></p>
+              <div className="border-solid border-2 border-gray-100">
+                <Link to={"/shop/" + products.author._id}>
+                  <p className="p-2">Sold by</p>
+                  <p className="text-xl p-2">{products.author.storeName}</p>
+                </Link>
+              </div>
             </div>
           </div>
-        </div>
+        </Modal>
       ) : (
         <Loading />
       )}
-    </Modal>
+    </div>
   );
 };
 
