065766e4a18a425fce37d396506add7f90801c52
MH Shuvo <mhshuvoalways@gmail.com>
2024-05-25 11:11:31 +0600

    refactor: review section change

--- client/src/components/Testimonials.js
+++ client/src/components/Testimonials.js
@@ -1,11 +1,11 @@
 import React, { useEffect } from "react";
 import ReactStars from "react-rating-stars-component";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllReviews } from "../store/actions/reviewAction";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import Loading from "../components/utils/Loading";
+import { getAllReviews } from "../store/actions/reviewAction";
 
 const ClientFeedBack = () => {
   const dispatch = useDispatch();
@@ -19,67 +19,64 @@ const ClientFeedBack = () => {
   const settings = {
     infinite: true,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
-            slidesToShow: 2,
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
-           slidesToShow: 1,
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   };
 
   return (
-    <div className="w-11/12 m-auto">
+    <>
       {reviewReducer.isLoading ? (
         <Loading />
       ) : (
-        <Slider {...settings}>
-          {reviewReducer.allreviews &&
-            [...reviewReducer.allreviews]
-              .reverse()
-              .map(
-                (el) =>
-                  el.authorId.avatar &&
-                  el.comments && (
-                    <div className="bg-gray-100 py-5">
-                      <img
-                        src={el.authorId.avatar.url}
-                        alt=""
-                        className="w-28 h-28 m-auto rounded-full"
-                      />
-                      <p className="text-center text-xl mt-2 font-semibold">
-                        {`${el.authorId.firstName} ${el.authorId.lastName}`}
-                      </p>
-                      <div className="flex justify-center mb-3">
-                        <ReactStars
-                          {...{
-                            size: 20,
-                            value: el.rating,
-                            count: 5,
-                            activeColor: "red",
-                            a11y: true,
-                            isHalf: true,
-                            edit: false,
-                          }}
-                        />
-                      </div>
-                      <p className="text-center px-5">{el.comments}</p>
-                    </div>
-                  )
-              )}
+        <Slider {...settings} className="!w-auto">
+          {[...reviewReducer.allreviews].reverse().map(
+            (el) =>
+              el.authorId.avatar &&
+              el.comments && (
+                <div className="bg-gray-100 py-5 h-72 shadow !w-5/12 mx-auto">
+                  <img
+                    src={el.authorId.avatar.url}
+                    alt=""
+                    className="w-28 h-28 m-auto rounded-full"
+                  />
+                  <p className="text-center text-xl mt-2 font-semibold">
+                    {`${el.authorId.firstName} ${el.authorId.lastName}`}
+                  </p>
+                  <div className="flex justify-center mb-3">
+                    <ReactStars
+                      {...{
+                        size: 20,
+                        value: el.rating,
+                        count: 5,
+                        activeColor: "red",
+                        a11y: true,
+                        isHalf: true,
+                        edit: false,
+                      }}
+                    />
+                  </div>
+                  <p className="text-center px-5 line-camp-2">{el.comments}</p>
+                </div>
+              )
+          )}
         </Slider>
       )}
-    </div>
+    </>
   );
 };
 
--- client/src/utils/backendUrl.js
+++ client/src/utils/backendUrl.js
@@ -1,6 +1,6 @@
 const backendUrl =
   process.env.NODE_ENV === "production"
     ? process.env.REACT_APP_BACKENDURL_PRODUCTION
-    : process.env.REACT_APP_BACKENDURL_DEV;
+    : process.env.REACT_APP_BACKENDURL_PRODUCTION;
 
 export default backendUrl;
