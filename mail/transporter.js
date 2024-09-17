16248ffe3655fc0e5d3d19e4bf061607c72679a4
MH Shuvo <mhshuvoalways@gmail.com>
2024-09-17 23:06:25 +0600

    refactor: transfer api to vercel

--- mail/transporter.js
+++ mail/transporter.js
@@ -1,20 +1,19 @@
-const nodemailer = require("nodemailer");
+const { Resend } = require("resend");
 
 module.exports = (sendto, templateName, name, link) => {
-  const transporter = nodemailer.createTransport({
-    service: "SendinBlue",
-    auth: {
-      user: process.env.SENDINBLUE_USER,
-      pass: process.env.SENDINBLUE_PASS,
-    },
-  });
+  const resend = new Resend(process.env.RESEND_KEY);
 
-  const mailOptions = {
-    from: process.env.SENDINBLUE_USER,
-    to: sendto,
-    subject: "e-Shop",
-    html: templateName(name, link),
-  };
-
-  transporter.sendMail(mailOptions);
+  resend.emails
+    .send({
+      from: "Portfolio <noreply@mhshuvo.com>",
+      to: sendto,
+      subject: `A message from my portfolio!`,
+      html: templateName(name, link),
+    })
+    .then(() => {
+      res.send("Message sent to admin email");
+    })
+    .catch(() => {
+      res.send("Something is wrong");
+    });
 };
